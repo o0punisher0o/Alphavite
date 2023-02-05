a36a175b377f4483a4ad6718e5d8c8ef28640f96
Akhmetov Kalizhan <100195237+o0punisher0o@users.noreply.github.com>
2023-02-06 03:16:44 +0600

    delete

--- app.py
+++
@@ -1,8 +0,0 @@
-import requests, psycopg2
-from flask import Flask, render_template, url_for, request
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-
-from sweater import app, __name__
-
-if __name__ == "__main__":
-    app.run(debug=True)
